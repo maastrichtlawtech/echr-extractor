baca63e3edf85c79f75e193b1e757da277b13654
shashankmc <shashank.chakravarthy@gmail.com>
2026-03-23 16:52:49 +0100

    refactor: add missing newline for improved readability in get_full_text_from_html

--- src/echr_extractor/ECHR_html_downloader.py
+++ src/echr_extractor/ECHR_html_downloader.py
@@ -14,6 +14,7 @@ def get_full_text_from_html(html_text):
     soup = BeautifulSoup(html_text, "html.parser")
     for script in soup(["script", "style"]):
         script.extract()  # rip it out
+
     def _extract_text(tag):
         br_token = "__HUDOC_BR__"
         for br in tag.find_all("br"):
